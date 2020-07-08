372a2d3b5535f37dbdc893b134c00cfef7a1bbc0
HBKDK <hbkdk83@gmail.com>
2020-07-09 00:14:36 +0900

    Commit Where_Am_I Project Results
    Feat. Add new project Where_Am_I

---
+++ catkin_ws/devel/share/gennodejs/ros/move_base_msgs/msg/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let MoveBaseActionFeedback = require('./MoveBaseActionFeedback.js');
+let MoveBaseAction = require('./MoveBaseAction.js');
+let MoveBaseGoal = require('./MoveBaseGoal.js');
+let MoveBaseActionGoal = require('./MoveBaseActionGoal.js');
+let MoveBaseResult = require('./MoveBaseResult.js');
+let MoveBaseFeedback = require('./MoveBaseFeedback.js');
+let MoveBaseActionResult = require('./MoveBaseActionResult.js');
+
+module.exports = {
+  MoveBaseActionFeedback: MoveBaseActionFeedback,
+  MoveBaseAction: MoveBaseAction,
+  MoveBaseGoal: MoveBaseGoal,
+  MoveBaseActionGoal: MoveBaseActionGoal,
+  MoveBaseResult: MoveBaseResult,
+  MoveBaseFeedback: MoveBaseFeedback,
+  MoveBaseActionResult: MoveBaseActionResult,
+};
